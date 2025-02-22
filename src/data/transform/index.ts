e2f2eac8f0a3a583eb452f4e7dd4a286ceb3a79d
Deshen Rao <deshen.r1502@gmail.com>
2025-02-22 16:33:50 +0100

    feat(transform): add composable pipeline and chainable wrapper

---
+++ src/data/transform/index.ts
@@ -0,0 +1,7 @@
+export * from './types.ts';
+export * from './basics.ts';
+export * from './aggregate.ts';
+export * from './bin.ts';
+export * from './join.ts';
+export * from './reshape.ts';
+export * from './pipeline.ts';
---
+++ src/data/transform/pipeline.ts
@@ -0,0 +1,39 @@
+import type { Dataset, Row } from '../../types/data.ts';
+import type { Transform } from './types.ts';
+
+export function pipeline(...transforms: readonly Transform[]): Transform {
+  if (transforms.length === 0) return (dataset) => dataset;
+  if (transforms.length === 1) return transforms[0] as Transform;
+  return (dataset) => {
+    let current = dataset;
+    for (const transform of transforms) current = transform(current);
+    return current;
+  };
+}
+
+export class Pipe {
+  private constructor(private readonly dataset: Dataset) {}
+
+  static from(dataset: Dataset): Pipe {
+    return new Pipe(dataset);
+  }
+
+  apply(...transforms: readonly Transform[]): Pipe {
+    return new Pipe(pipeline(...transforms)(this.dataset));
+  }
+
+  tap(visitor: (dataset: Dataset) => void): Pipe {
+    visitor(this.dataset);
+    return this;
+  }
+
+  toDataset(): Dataset {
+    return this.dataset;
+  }
+
+  toRows(): readonly Row[] {
+    return this.dataset.rows;
+  }
+}
+
+export const from = Pipe.from;
