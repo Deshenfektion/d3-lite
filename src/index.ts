60212ec8c73e6e7e742e2b150721ad3076b1b1be
Deshen Rao <deshen.r1502@gmail.com>
2025-03-16 12:04:18 +0100

    feat(core): export scale and color layers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './utils/index.ts';
 export * from './data/index.ts';
+export * from './interpolate/index.ts';
+export * from './color/index.ts';
+export * from './scales/index.ts';
 export type * from './types/index.ts';
